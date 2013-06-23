96dc378a1fa36ea715e7cc58a68510547eaaef12
Terentev Evgeny <terentev@gmail.com>
2013-06-23 12:06:32 +0400

    some

--- src/main/java/com/github/terentev/tolato/interfaces/Towriter.java
+++ src/main/java/com/github/terentev/tolato/interfaces/Towriter.java
@@ -1,6 +1,8 @@
 package com.github.terentev.tolato.interfaces;
 
 public interface Towriter {
+    void writeBitInt(int value);
+
     void writeBit(boolean value);
 
     void writeTags(int[] tags);
